b92e6faf4d0001ff1c60ad6f81d383acddf46a41
SForschen <serenity_may@yahoo.com>
2024-02-14 15:36:06 -0700

    columns

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,6 +1,14 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  const imgColClass = 'columns-img-col';
+
+  const isTeaser = block.classList.contains('teaser');
+
+  // teaser is also a variation of thirds
+  if (isTeaser) {
+    block.classList.add('thirds');
+  }
 
   // setup image columns
   [...block.children].forEach((row) => {
@@ -8,9 +16,17 @@ export default function decorate(block) {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
+        // less than or equals to two in case image is wrapped with <a> tag
+        if (picWrapper && picWrapper.children.length <= 2) {
           // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
+          picWrapper.classList.add(imgColClass);
+        }
+
+        if (isTeaser) {
+          const teaserWrapper = createElement('div', 'teaser-wrapper');
+
+          teaserWrapper.appendChild(pic);
+          col.appendChild(teaserWrapper);
         }
       }
     });
