924753ea362558511f0f7acece847101576a0ad5
SForschen <serenity_may@yahoo.com>
2024-03-02 20:02:43 -0700

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -94,6 +94,37 @@ function buildAutoBlocks(main) {
   }
 }
 
+export function decorateLinks(element) {
+  const hosts = ['localhost', 'hlx.page', 'hlx.live'];
+  element.querySelectorAll('a').forEach((a) => {
+    try {
+      if (a.href) {
+        const url = new URL(a.href);
+
+        // local links are relative
+        // non local and non email links open in a new tab
+        const hostMatch = hosts.some((host) => url.hostname.includes(host));
+        const emailMatch = a.href.includes('mailto');
+
+        if (hostMatch) {
+          a.href = `${url.pathname.replace('.html', '')}${url.search}${url.hash}`;
+        } else if (!emailMatch) {
+          a.target = '_blank';
+          a.rel = 'noopener noreferrer';
+          const icon = createElement('span', ['icon', 'icon-external-link']);
+          a.insertAdjacentElement('beforeend', icon);
+          const linkTitle = a.title;
+          a.title = linkTitle ? `${linkTitle} (opens an external site)` : 'Link opens an external site';
+        }
+      }
+    } catch (e) {
+      // something went wrong
+      // eslint-disable-next-line no-console
+      console.log(e);
+    }
+  });
+}
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
@@ -106,6 +137,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  decorateLinks(main);
 }
 
 /**
@@ -153,60 +185,6 @@ async function loadLazy(doc) {
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
-}
-
-/**
- * inspects the url, if url has a special file type return the file type, otherwise return null
- *
- * Note: urls to non-HTML files should display the file type in the hyperlink
- * This way users know they are downloading a file.
- * @param {string} url the url that may or may not link to a display file type
- */
-function getDisplayFileTypeFromUrl(url) {
-  const displayFileTypes = ['pdf', 'docx'];
-  const urlExt = url.substring(url.lastIndexOf('.'));
-  const regex = new RegExp(`(${displayFileTypes.join('|')})`, 'g');
-
-  const fileTypeArr = urlExt.toLowerCase().match(regex);
-  return (fileTypeArr && fileTypeArr.length) ? fileTypeArr[0].toUpperCase() : null;
-}
-
-export function decorateLinks(element) {
-  const hosts = ['localhost', 'hlx.page', 'hlx.live'];
-  element.querySelectorAll('a').forEach((a) => {
-    try {
-      if (a.href) {
-        const url = new URL(a.href);
-
-        // check of link needs to display file type
-        const displayFileType = getDisplayFileTypeFromUrl(a.href);
-        if (displayFileType) {
-          a.innerText += ` (${displayFileType})`;
-          a.target = '_blank';
-        }
-
-        // local links are relative
-        // non local and non email links open in a new tab
-        const hostMatch = hosts.some((host) => url.hostname.includes(host));
-        const emailMatch = a.href.includes('mailto');
-
-        if (hostMatch) {
-          a.href = `${url.pathname.replace('.html', '')}${url.search}${url.hash}`;
-        } else if (!emailMatch) {
-          a.target = '_blank';
-          a.rel = 'noopener noreferrer';
-          const icon = createElement('span', ['icon', 'icon-external-link']);
-          a.insertAdjacentElement('beforeend', icon);
-          const linkTitle = a.title;
-          a.title = linkTitle ? `${linkTitle} (opens an external site)` : 'Link opens an external site';
-        }
-      }
-    } catch (e) {
-      // something went wrong
-      // eslint-disable-next-line no-console
-      console.log(e);
-    }
-  });
 }
 
 /**
