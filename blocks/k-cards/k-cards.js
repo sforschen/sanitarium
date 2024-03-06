35a6c0461b7c287a00ed5b4cc0e6b7eac8e6e814
SForschen <serenity_may@yahoo.com>
2024-03-06 16:39:18 -0700

    update

--- blocks/k-cards/k-cards.js
+++ blocks/k-cards/k-cards.js
@@ -1,5 +1,33 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
+// Function to check if the text contains a number followed by a percentage symbol
+function containsNumberPercentage(text) {
+  const regex = /\d+%/;
+  return regex.test(text);
+}
+
+// Function to extract the percentage value from the text
+function getPercentageValue(text) {
+  const regex = /\d+/;
+  const match = text.match(regex);
+  return match ? parseInt(match[0], 10) : 0;
+}
+
+// Function to get the background color based on the percentage value
+function getBackgroundColor(percentageValue) {
+  if (percentageValue >= 0 && percentageValue <= 20) {
+    return 'var(--clr-blue)';
+  } else if (percentageValue > 20 && percentageValue <= 40) {
+    return 'var(--clr-red)';
+  } else if (percentageValue > 40 && percentageValue <= 60) {
+    return 'var(--clr-orange)';
+  } else if (percentageValue > 60 && percentageValue <= 80) {
+    return 'var(--clr-light-blue)';
+  } else {
+    return 'var(--clr-green)';
+  }
+}
+
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
@@ -23,32 +51,4 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
-}
-
-// Function to check if the text contains a number followed by a percentage symbol
-function containsNumberPercentage(text) {
-  const regex = /\d+%/;
-  return regex.test(text);
-}
-
-// Function to extract the percentage value from the text
-function getPercentageValue(text) {
-  const regex = /\d+/;
-  const match = text.match(regex);
-  return match ? parseInt(match[0]) : 0;
-}
-
-// Function to get the background color based on the percentage value
-function getBackgroundColor(percentageValue) {
-  if (percentageValue >= 0 && percentageValue <= 20) {
-    return 'var(--clr-blue)';
-  } else if (percentageValue > 20 && percentageValue <= 40) {
-    return 'var(--clr-red)';
-  } else if (percentageValue > 40 && percentageValue <= 60) {
-    return 'var(--clr-orange)';
-  } else if (percentageValue > 60 && percentageValue <= 80) {
-    return 'var(--clr-light-blue)';
-  } else {
-    return 'var(--clr-green)';
-  }
 }
